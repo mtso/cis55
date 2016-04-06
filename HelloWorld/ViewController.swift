6057a3eea2484e37185254087e82341e22b36e78
mtso <matthewtso@gmail.com>
2016-04-06 12:55:42 -0700

    Adding Hello World project.

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Matthew Tso on 4/4/16.
+//  Copyright © 2016 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var labelText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func helloWorldClick(sender: AnyObject) {
+        let clickAlert = UIAlertController(title: "Matthew's Hello World", message: "Hello World app works with the Alert Controller.", preferredStyle: UIAlertControllerStyle.Alert)
+        clickAlert.addAction(UIAlertAction(title: "Great!", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(clickAlert, animated: true, completion: nil)
+        
+        labelText.text = "Button Clicked!"
+    }
+
+}
+
