50e8293685e82a0f587a0f668966491293db3279
mtso <matthewtso@gmail.com>
2016-05-02 17:58:47 -0700

    Add knapsack

---
+++ Algorithms/Knapsack.playground/Contents.swift
@@ -0,0 +1,11 @@
+/*
+  
+  0-1 Knapsack Problem
+
+  Given weights and values of n items, put these items in a knapsack 
+  of capacity W to get the maximum total value in the knapsack.
+
+*/
+
+func knapsack(capacity, values, weights, ) -> Int {
+}
